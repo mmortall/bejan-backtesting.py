mmortall/bejan-backtesting.py

commit 29dd179d406bbdbd0d2ded0abca7e2116ce1ff00

BUG: Fix plot not saved as specified filename if run within notebook
Fixes https://github.com/kernc/backtesting.py/issues/45

backtesting/_plotting.py
@@ -49,14 +49,12 @@ with open(os.path.join(os.path.dirname(__file__), 'autoscale_cb.js'),
 
 def _bokeh_reset(filename=None):
     curstate().reset()
-    # Test if we are in Jupyter notebook
-    if IS_JUPYTER_NOTEBOOK:
-        curstate().output_notebook()
-    elif filename:
+    if filename:
         if not filename.endswith('.html'):
             filename += '.html'
-
         output_file(filename, title=filename)
+    elif IS_JUPYTER_NOTEBOOK:
+        curstate().output_notebook()
 
 
 def colorgen():

commit 69593aae1d541c7e3adf117eb3009d58da20fdd4

BUG: Fix plot saving to filename regression since 29dd179d406bbdbd0d
The plot would save to filename even when run in notebook. Argh.

Refs: https://github.com/kernc/backtesting.py/issues/45

backtesting/__init__.py
@@ -41,3 +41,4 @@ except ImportError:
 
 from .backtesting import Backtest, Strategy, Orders, Position   # noqa: F401
 from . import lib  # noqa: F401
+from ._plotting import set_bokeh_output  # noqa: F401

backtesting/_plotting.py
@@ -1,4 +1,6 @@
 import os
+import re
+import sys
 import warnings
 from itertools import cycle, combinations
 from functools import partial
@@ -31,6 +33,11 @@ from bokeh.transform import factor_cmap
 
 from backtesting._util import _data_period, _as_list
 
+
+with open(os.path.join(os.path.dirname(__file__), 'autoscale_cb.js'),
+          encoding='utf-8') as _f:
+    _AUTOSCALE_JS_CALLBACK = _f.read()
+
 IS_JUPYTER_NOTEBOOK = 'JPY_PARENT_PID' in os.environ
 
 if IS_JUPYTER_NOTEBOOK:
@@ -38,13 +45,24 @@ if IS_JUPYTER_NOTEBOOK:
                   'Setting Bokeh output to notebook. '
                   'This may not work in Jupyter clients without JavaScript '
                   'support (e.g. PyCharm, Spyder IDE). '
-                  'Reset with `bokeh.io.reset_output()`.')
+                  'Reset with `backtesting.set_bokeh_output(notebook=False)`.')
     output_notebook()
 
 
-with open(os.path.join(os.path.dirname(__file__), 'autoscale_cb.js'),
-          encoding='utf-8') as _f:
-    _AUTOSCALE_JS_CALLBACK = _f.read()
+def set_bokeh_output(notebook=False):
+    """
+    Set Bokeh to output either to a file or Jupyter notebook.
+    By default, Bokeh outputs to notebook if running from within
+    notebook was detected.
+    """
+    global IS_JUPYTER_NOTEBOOK
+    IS_JUPYTER_NOTEBOOK = notebook
+
+
+def _windos_safe_filename(filename):
+    if sys.platform.startswith('win'):
+        return re.sub(r'[^a-zA-Z0-9,_-]', '_', filename.replace('=', '-'))
+    return filename
 
 
 def _bokeh_reset(filename=None):
@@ -75,10 +93,11 @@ def plot(*, results, df, indicators, filename='', plot_width=None,
     """
     Like much of GUI code everywhere, this is a mess.
     """
-
     # We need to reset global Bokeh state, otherwise subsequent runs of
     # plot() contain some previous run's cruft data (was noticed when
     # TestPlot.test_file_size() test was failing).
+    if not filename and not IS_JUPYTER_NOTEBOOK:
+        filename = _windos_safe_filename(str(results._strategy))
     _bokeh_reset(filename)
 
     COLORS = [BEAR_COLOR, BULL_COLOR]
@@ -570,9 +589,7 @@ def plot_heatmaps(heatmap: pd.Series, agg: str, ncols: int,
         raise ValueError('heatmap must be heatmap Series as returned by '
                          '`Backtest.optimize(..., return_heatmap=True)`')
 
-    _bokeh_reset()
-    if filename:
-        output_file(filename)
+    _bokeh_reset(filename)
 
     param_combinations = combinations(heatmap.index.names, 2)
     dfs = [heatmap.groupby(list(dims)).agg(agg).to_frame(name='_Value')

backtesting/backtesting.py
@@ -7,8 +7,6 @@ module directly, e.g.
 """
 import multiprocessing as mp
 import os
-import re
-import sys
 import warnings
 from abc import abstractmethod, ABCMeta
 from collections.abc import Sequence
@@ -999,16 +997,11 @@ class Backtest:
                 raise RuntimeError('First issue `backtest.run()` to obtain results.')
             results = self._results
 
-        def _windos_safe_filename(filename):
-            if sys.platform.startswith('win'):
-                return re.sub(r'[^a-zA-Z0-9,_-]', '_', filename.replace('=', '-'))
-            return filename
-
         plot(
             results=results,
             df=self._data,
             indicators=results._strategy._indicators,
-            filename=filename or _windos_safe_filename(str(results._strategy)),
+            filename=filename,
             plot_width=plot_width,
             plot_equity=plot_equity,
             plot_pl=plot_pl,